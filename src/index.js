f62263937077a9ab71c3dcff7547bcf320725464
Jared S <jaredjames2020@gmail.com>
2020-04-06 17:34:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-let addToy = false;
+// let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-    } else {
-      toyForm.style.display = "none";
-    }
-  });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const addBtn = document.querySelector("#new-toy-btn");
+//   const toyForm = document.querySelector(".container");
+//   addBtn.addEventListener("click", () => {
+//     // hide & seek with the form
+//     addToy = !addToy;
+//     if (addToy) {
+//       toyForm.style.display = "block";
+//     } else {
+//       toyForm.style.display = "none";
+//     }
+//   });
+// });
